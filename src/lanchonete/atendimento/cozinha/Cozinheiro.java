3b831c0710fec83d10c8a1cfd1b1a13d54ea481f
DarieldonMedeiros <darieldonbm99@outlook.com>
2025-08-31 09:06:13 -0300

    Initial Commit

---
+++ src/lanchonete/atendimento/cozinha/Cozinheiro.java
@@ -0,0 +1,61 @@
+package lanchonete;
+
+public class Cozinheiro {
+    public void adicionarLancheNoBalcao(){
+        System.out.println("ADICIONANDO LANCHE NATURAL HAMBURGER NO BALCÃO");
+    }
+
+    public void adicionarSucoNoBalcao(){
+        System.out.println("ADICIONANDO SUCO NO BALCÃO");
+    }
+
+    public void adicionarComboNoBalcao(){
+        adicionarLancheNoBalcao();
+        adicionarSucoNoBalcao();
+    }
+
+    private void prepararLanche(){
+        System.out.println("PREPARANDO LANCHE TIPO HAMBURGER");
+    }
+
+    private void prepararVitamina(){
+        System.out.println("PREPARANDO SUCO");
+    }
+
+    private void prepararCombo(){
+        prepararLanche();
+        prepararVitamina();
+    }
+
+    public void selecionarIngredientesLanche(){
+        System.out.println("SELECIONANDO O PÃO, SALADA, OVO E CARNE");
+    }
+
+    private void selecionarIngredientesVitamina(){
+        System.out.println("SELECIONANDO FRUTA, LEITE E SUCO");
+    }
+
+    private void lavarIngredientes(){
+        System.out.println("LAVANDO INGREDIENTES");
+    }
+
+    private void baterVitaminaLiquidificador(){
+        System.out.println("BATENDO VITAMINA LIQUIDIFICADOR");
+    }
+
+    private void fritarIngredientesLanche(){
+        System.out.println("FRITANDO A CARNE E OVO PARA HAMBURGER");
+    }
+
+    private void pedirParaTrocarGas(Atendente meuAmigo){
+        meuAmigo.trocarGas();
+    }
+
+    private void pedirParaTrocarGas(Almoxarife meuAmigo){
+        meuAmigo.trocarGas();
+    }
+
+    private void pedirIngredientes(Almoxarife almoxarife){
+        almoxarife.entregarIngredientes();
+    }
+}
